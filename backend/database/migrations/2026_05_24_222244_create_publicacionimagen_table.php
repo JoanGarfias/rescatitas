79f1656f943595f1747d408fb7a640c7c7eff2e0
Joan Garfias <joanpagarf09@gmail.com>
2026-05-24 16:23:58 -0600

    create post images (for r2 service)

---
+++ backend/database/migrations/2026_05_24_222244_create_publicacionimagen_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publicacion_imagen', function (Blueprint $table) {
+            $table->id('id_publicacion_imagen');
+            $table->unsignedBigInteger('id_publicacion');
+            $table->foreign('id_publicacion')
+                ->references('id_publicacion')->on('publicacion')
+                ->onDelete('cascade');
+            $table->string('ruta_imagen', 256);
+            $table->text('url_imagen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publicacion_imagen');
+    }
+};
