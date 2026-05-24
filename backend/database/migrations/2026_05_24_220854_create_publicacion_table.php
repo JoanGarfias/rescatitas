028bf9e989e88a0d197d8d70d10e5c4378c35794
Joan Garfias <joanpagarf09@gmail.com>
2026-05-24 16:17:45 -0600

    create publicacion table

---
+++ backend/database/migrations/2026_05_24_220854_create_publicacion_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publicacion', function (Blueprint $table) {
+            $table->id('id_publicacion');
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->string('nombre_mascota', 128);
+            $table->text('descripcion');
+            $table->date('fecha_desaparicion');
+            $table->string('lugar_desaparicion', 256)->nullable();
+            $table->decimal('latitud', 10, 7)->nullable();
+            $table->decimal('longitud', 10, 7)->nullable();
+            //Pendiente apodos en array (sql)
+            $table->enum('estado_mascota', [
+                'perdido',
+                'encontrado',
+                'rescatado',
+            ]);
+            $table->enum('genero', ['macho', 'hembra', 'desconocido']);
+            $table->enum('estado_publicacion', [
+                'activa',
+                'cerrada',
+                'archivada'
+            ])->default('activa');
+            $table->enum('tipo_publicacion', [
+                'perdido',
+                'encontrado'
+            ])->default('perdido');
+            $table->unsignedBigInteger('id_rescatista')->nullable();
+            $table->foreign('id_rescatista')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publicacion');
+    }
+};
