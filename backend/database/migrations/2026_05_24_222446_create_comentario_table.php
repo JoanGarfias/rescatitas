7f8ec5fe15ce12198b433bd8668cee4e053f9100
Joan Garfias <joanpagarf09@gmail.com>
2026-05-24 16:25:45 -0600

    create comentario table

---
+++ backend/database/migrations/2026_05_24_222446_create_comentario_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comentario', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_publicacion');
+            $table->foreign('id_publicacion')
+                ->references('id_publicacion')->on('publicacion')
+                ->onDelete('cascade');
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->text('contenido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comentario');
+    }
+};
