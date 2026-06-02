db805a7e7611d59436fcbedf7f055e977a2408f9
Joan Garfias <joanpagarf09@gmail.com>
2026-06-01 22:40:11 -0600

    add base controller

---
+++ backend/app/Http/Controllers/BaseController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class BaseController extends Controller
+{
+    protected function currentUser(): ?User 
+    {
+        return request()->user();
+    }
+
+    protected function isAuthenticated(): bool 
+    {
+        return request()->auth()->check();
+    }
+
+    protected function currentUserId(): ?int 
+    {
+        return request()->user()?->id;
+    }
+}
