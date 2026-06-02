60612498f1a9f8a499b6f18beba80e6d8f95fa6f
Joan Garfias <joanpagarf09@gmail.com>
2026-06-01 22:42:22 -0600

    feat: pet posts

---
+++ backend/app/Http/Controllers/PetController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterPetPostRequest;
+use App\Services\PetService;
+use Exception;
+use Illuminate\Http\Request;
+
+class PetController extends BaseController
+{
+    protected PetService $petService;
+
+    public function __construct(PetService $petService)
+    {
+        $this->petService = $petService;
+    }
+
+    public function search(Request $request)
+    {
+        $nombre = $request->input('nombre');
+        $direccion = $request->input('direccion');
+        $results = $this->petService->searchPets($nombre, $direccion);
+
+        return response()->json($results);
+    }
+
+    public function store(RegisterPetPostRequest $request)
+    {
+
+        $data = $request->validated();
+
+        $data['id_usuario'] = $this->currentUserId(); 
+        $data['estado_mascota'] = 'perdido';
+        $data['estado_publicacion'] = 'activa';
+        $data['fecha_registro'] = now();
+
+        try{
+            $image = $request->file('imagen');
+            $pet = $this->petService->registerPet($data, $image);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Error al registrar la mascota'
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Mascota registrada exitosamente',
+            'pet' => $pet
+        ], 201);
+    }
+
+    public function recent()
+    {
+        $pets = $this->petService->getRecentPets();
+
+        return response()->json([
+            'pets' => $pets
+        ]);
+    }
+
+    public function show(int $id)
+    {
+        $pet = $this->petService->getPetById($id);
+
+        if (!$pet) {
+            return response()->json([
+                'message' => 'Mascota no encontrada'
+            ], 404);
+        }
+
+        return response()->json([
+            'pet' => $pet
+        ]);
+    }
+
+    public function getPets(Request $request){
+
+        $filters = $request->only(['nombre_mascota', 'direccion', 'raza', 'genero', 'tipo_mascota']);
+
+        $pets = $this->petService->getPets($filters);
+
+        return response()->json([
+            'pets' => $pets
+        ]);
+    }
+
+    public function destroy(int $id){
+        $pet = $this->petService->getPetById($id);
+        if (!$pet) {
+            return response()->json([
+                'message' => 'Mascota no encontrada'
+            ], 404);
+        }
+        $canDelete = $this->petService->destroy($id);
+        if (!$canDelete) {
+            return response()->json([
+                'message' => 'Error al eliminar la mascota, esta mascota ya no está registrada'
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Mascota eliminada exitosamente'
+        ]);
+    }
+
+}
---
+++ backend/app/Services/PetService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Publicacion;
+use Illuminate\Http\UploadedFile;
+
+class PetService
+{
+    public function searchPets(string | null $nombre, string | null $direccion)
+    {
+        $query = Publicacion::query();
+        if($nombre){
+            $query->where('nombre_mascota', 'like', '%' . $nombre . '%');
+        }
+        if($direccion){
+            $query->orWhere('direccion', 'like', '%' . $direccion . '%');
+        }
+        $results = $query->get();
+
+        return $results;
+    }
+
+    public function registerPet(array $data, ?UploadedFile $image = null)
+    {
+        if ($image) {
+            //guardamos en disco local, luego guardamos en R2
+            $data['imagen'] = $image->store('pets', 'public');
+        }
+        $pet = Publicacion::create($data);
+        return $pet;
+    }
+
+    public function getRecentPets()
+    {
+        return Publicacion::orderBy('created_at', 'desc')->take(6)->get();
+    }
+
+    public function getPetById(int $id)
+    {
+        return Publicacion::find($id);
+    }
+
+    public function getPets(array $filters){
+        $query = Publicacion::query();
+
+        if (isset($filters['nombre_mascota'])) {
+            $query->where('nombre_mascota', 'like', '%' . $filters['nombre_mascota'] . '%');
+        }
+
+        if (isset($filters['direccion'])) {
+            $query->where('direccion', 'like', '%' . $filters['direccion'] . '%');
+        }
+
+        if (isset($filters['raza'])) {
+            $query->where('raza', 'like', '%' . $filters['raza'] . '%');
+        }
+
+        if (isset($filters['genero'])) {
+            $query->where('genero', '=', $filters['genero']);
+        }
+
+        return $query->get();
+    }
+
+    public function destroy(int $id)
+    {
+        $pet = Publicacion::find($id);
+        if ($pet) {
+            $pet->delete();
+            return true;
+        }
+        return false;
+    }
+}
